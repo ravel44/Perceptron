d5190a94ea887ac593ba210a84e38ff7cfc9a54c
Pascal Guyon <31578450+ravel44@users.noreply.github.com>
2019-06-10 20:53:29 -0700

    cleaning

--- bubbles.js
+++ bubbles.js
@@ -3,22 +3,15 @@ function Bubble(x, y){
   this.y=random(0, height);
   this.correctClassification;
   
-  //correct answer definition
   if(this.x>this.y){
     this.correctClassification=1;
   }else{
     this.correctClassification=-1;
   }
-  
 
   this.show =function(){
     stroke(0);
     fill(255);
     ellipse(this.x, this.y, 32, 32);
   }
-  
-  // this.color = function(){
-  //   fill(255);
-  //   ellipse(this.x, this.y, 16,16);
-  // }
 }
